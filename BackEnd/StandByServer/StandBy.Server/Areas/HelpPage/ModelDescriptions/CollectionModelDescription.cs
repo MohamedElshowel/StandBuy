0d4ad00d7898d8c201096aec9191a755edab6f7b
AmrFayez <34596347+AmrFayez@users.noreply.github.com>
2019-12-06 14:44:18 +0200

    Init FE and BE
    * init server project
    
    * add cors
    
    * add swagger
    
    * add git ignore
    
    * password validator
    
    * add angular project
    
    * update gitignore

---
+++ BackEnd/StandByServer/StandBy.Server/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace StandBy.Server.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ BackEnd/StandByServer/StandBy.Server/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace StandBy.Server.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}

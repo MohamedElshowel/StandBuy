98524fcee0091af8d10dc80a201d0c8292f9e2e4
AmrFayez <amr.fayez1994@gmail.com>
2019-12-07 08:11:14 +0200

    shipment-component

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './core/home/home.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
 import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { PaymentMethodComponent } from './shipment/payment-method/payment-method.component';
+import { ShipmentMethodComponent } from './shipment/shipment-method/shipment-method.component';
+import { UserDetailsComponent } from './shipment/user-details/user-details.component';
 
 
 const routes: Routes = [
@@ -10,10 +13,10 @@ const routes: Routes = [
   {path :'home',component:HomeComponent},
   {path :'search',component:ProductListComponent},
   {path :'view',component:ProductDetailComponent},
+  {path :'payment-method',component:PaymentMethodComponent},
+  {path :'shipment-method',component:ShipmentMethodComponent},
+  {path :'user-details',component:UserDetailsComponent},
   {	path: '**',	component: HomeComponent}
-	
-	
-	
 	
 ];
 
